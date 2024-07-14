66aa77fdf3baaedfb3643a90c497646da2b014d3
Harikrishh123 <nvsatyamnwelamsetti@gmail.com>
2024-07-14 15:10:30 +0530

    update

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 
 const mongoose = require('mongoose')
 
-const mongUrl = "mongodb://localhost:27017/blog"
+const mongUrl = "mongodb+srv://hari_123:shhanuman@mydatabase.1had77f.mongodb.net/blog"
 
 const connectToMo = () => {
     mongoose.connect(mongUrl).then(console.log("mongoose is connected"))
